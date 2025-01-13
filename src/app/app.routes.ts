f4f6f592eb29c3d1e91a70910efd0f7852acf7ee
Adam <Adam.sturesson@live.se>
2025-01-13 21:18:25 +0100

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PicturesComponent } from './pictures/pictures.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent}
+    {path: '', component: HomeComponent},
+    {path: 'pictures', component: PicturesComponent},
 ];
 
 
