7c94dc74f04b37fe59da92ecdf9f550dc3d88d48
Adam <Adam.sturesson@live.se>
2025-02-01 10:06:16 +0100

    Updated picture html

--- src/app/pictures/pictures.component.ts
+++ src/app/pictures/pictures.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { PicturesService } from '../pictures.service';
 
 @Component({
   selector: 'app-pictures',
   standalone: true,
-  imports: [],
+  imports: [NgModule],
   templateUrl: './pictures.component.html',
   styleUrl: './pictures.component.css'
 })
